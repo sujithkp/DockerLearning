ed103e66f16b13af3496b0af6009aa6f7918dc18
Sujith <sujith.kp@hotmail.com>
2024-02-25 11:15:19 +0530

    sql connection check.

--- Product/ProductService/Controllers/ProductController.cs
+++ Product/ProductService/Controllers/ProductController.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using ProductService.Shared.Requests;
 using ProductService.Shared;
+using Microsoft.Data.SqlClient;
+using ProductService.Dal;
+using Microsoft.AspNetCore.Hosting.Server;
 
 namespace ProductService.Controllers
 {
@@ -22,6 +25,10 @@ namespace ProductService.Controllers
         [HttpGet(Name = "GetProduct")]
         public GetProductResponse Get()
         {
+            SqlConnection connection = new SqlConnection("Server = 192.168.29.52; Database = Northwind; User Id = sa; Password = Cbse#1728;Encrypt=false;TrustServerCertificate=true;");
+            connection.Open();
+
+
             var response = _productService.GetProducts(null);
 
             return response;
